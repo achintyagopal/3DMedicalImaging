9746c6620e81f53ddca7d7918dded839ebcaeaf3
billyonair <billyonair@acm.jhu.edu>
2016-12-03 16:24:37 -0500

    Pushing tests

---
+++ src/testProjection.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np 
+import Projection
+import pickle
+
+
+
+shape = [98, 512, 512]
+
+points = pickle.load("points.file")
+
+faces = pickle.load("faces.file")
+
+
+
+
+
