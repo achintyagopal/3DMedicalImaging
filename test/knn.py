7a5aaac0de2a436c229fd6d2fd3f6b3ef1bb94ae
Achintya Gopal <achintyagopal@Achintyas-MacBook-Air.local>
2016-12-04 16:41:46 -0500

    KNN (actually 1NN) code

--- test/knn.py
+++ test/knn.py
@@ -1,7 +1,26 @@
+import numpy as np
+import instance
+
+
 class KNN():
 
+    def __init__(self):
+        self.instances = None
+
     def train(self, instances):
-        pass
+        self.instances = instances
 
     def test(self, instances):
-        pass
+        labels = []
+        for instance in instances:
+            max_dist = None
+            max_index = None
+            i = 0
+            for test_inst in self.instances:
+                dist = np.linalg.norm(test_inst.get_vector() - instance.get_vector())
+                if max_index is None or dist > max_dist:
+                    dist = max_dist
+                    max_index = i
+                i += 1
+            labels.append(instance[max_index].get_label())
+        return labels
