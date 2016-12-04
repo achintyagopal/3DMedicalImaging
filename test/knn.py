5771389a641740331907e4844518f14a1d121d45
Achintya Gopal <achintyagopal@Achintyas-MacBook-Air.local>
2016-12-04 18:28:12 -0500

    Min dist not max dist

--- test/knn.py
+++ test/knn.py
@@ -1,7 +1,6 @@
 import numpy as np
 import instance
 
-
 class KNN():
 
     def __init__(self):
@@ -13,14 +12,14 @@ class KNN():
     def test(self, instances):
         labels = []
         for instance in instances:
-            max_dist = None
-            max_index = None
+            min_dist = None
+            min_index = None
             i = 0
             for test_inst in self.instances:
                 dist = np.linalg.norm(test_inst.get_vector() - instance.get_vector())
-                if max_index is None or dist > max_dist:
-                    dist = max_dist
-                    max_index = i
+                if min_index is None or dist < min_dist:
+                    dist = min_dist
+                    min_index = i
                 i += 1
-            labels.append(instance[max_index].get_label())
+            labels.append(instance[min_index].get_label())
         return labels
